539c13a4a601f4a4e183c6762b6c05c496c56439
sjasthi <siva.jasthi@metrostate.edu>
2021-07-29 20:47:45 -0500

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+	Run Anagram.java
+	Menu interface is presented to the user
+1	Build Anagram Collections
+2	Find path between two words
+3	Find all paths from a word with N steps
+4	Find a path from a word with 10 steps and generate puzzle for that. (in HTML format)
+5	Exit
+
+1.
+Success: Anagrams are built from the database
+	
+	2
+	Enter the first word:
+	Enter the second word:
+	
+	3
+	Enter the word:
+	Enter the number of steps:
+	
+	4
+	Enter the word:
+	
+	5
+	Good bye!
