ba1ab12f32e73d40db56a9269d95938e692daf11
ramlamohamed-github <84943621+ramlamohamed-github@users.noreply.github.com>
2021-08-08 15:37:42 -0500

    Update ladder.java

--- ladder.java
+++ ladder.java
@@ -427,7 +427,6 @@ public class ladder extends databaseInfo {
 				
 				}
 		
-			dictionaryWords.replaceAll(String::toUpperCase);
 
 			
 			  int startpoint4 = dictionaryWords.indexOf(startValue4);
